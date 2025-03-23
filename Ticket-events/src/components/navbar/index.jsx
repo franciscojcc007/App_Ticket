f5a2bdf92efa30383f2088c795cb9381632ec4e7
franciscoj <frankmazta@gmail.com>
2025-03-23 12:08:31 -0400

    Agregar funcionalidad de búsqueda en la barra de navegación

--- Ticket-events/src/components/navbar/index.jsx
+++ Ticket-events/src/components/navbar/index.jsx
@@ -1,8 +1,27 @@
-const Navbar = ()=>{
-  return(
+import { useState } from 'react';
+
+const Navbar = ({onSearch}) => {
+  const [search, setSearch] = useState('')
+
+  const handInputChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const handleInputKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      onSearch(search)
+    }
+  }
+
+  return (
     <div>
       <p>Eventos</p>
-      <input placeholder="Busca tu evento favorito"/>
+      <input
+        placeholder="Busca tu evento favorito"
+        onChange={handInputChange}
+        onKeyDown={handleInputKeyDown}
+        value={search}
+      />
     </div>
   )
 }
