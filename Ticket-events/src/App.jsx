b582fe91ea2513f0af82f22d94bc1cd704fdecdf
franciscoj <frankmazta@gmail.com>
2025-03-23 09:26:38 -0400

    Agregar componente Events a la aplicación

--- Ticket-events/src/App.jsx
+++ Ticket-events/src/App.jsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar'
+import Events from './components/Events/index.jsx'
 
 function App() {
 
   return (
     <>
       <Navbar />
+      <Events />
     </>
   )
 }
