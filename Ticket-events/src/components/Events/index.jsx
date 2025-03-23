7b9efef694fa61c05ae36e714265e05fce407b08
franciscoj <frankmazta@gmail.com>
2025-03-23 09:26:50 -0400

    Agregar lógica para renderizar eventos desde un archivo JSON

--- Ticket-events/src/components/Events/index.jsx
+++ Ticket-events/src/components/Events/index.jsx
@@ -1,11 +1,23 @@
 
+import EventItem from "./components/EventItem/index.jsx";
+import data from "../../data/events.json";
 
-const Events = () =>{
-  return(
-    <idv>
+const events = data._embedded.events;
+
+const Events = () => {
+  return (
+    <div>
       <h1>Eventos</h1>
-    </idv>
-  )
-}
+      {events.map((eventItem) => (
+        <EventItem
+          key={`event-item-${eventItem.id}`}
+          name={eventItem.name}
+          info={eventItem.info}
+          image={eventItem.images?.[0]?.url || 'ruta-a-imagen-default.jpg'}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Events;
