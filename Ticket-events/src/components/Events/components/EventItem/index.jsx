ee233b4f8ad2bfe228f09ad7ca430a999cf50497
franciscoj <frankmazta@gmail.com>
2025-03-23 12:08:24 -0400

    Agregar funcionalidad para manejar clics en el botón "Ver más" en el componente EventItem

--- Ticket-events/src/components/Events/components/EventItem/index.jsx
+++ Ticket-events/src/components/Events/components/EventItem/index.jsx
@@ -1,10 +1,20 @@
 
-const EventItem = ({name, info, image}) =>{
-  return(
-    <div>
+const EventItem = ({ name, info, id, image, onEventClick }) => {
+
+
+  const handleSeeMoreClick = () => {
+    (e) => {
+      e.stopPropagation()
+      onEventClick(id)
+    }
+  }
+
+  return (
+    <div >
       <img src={image} alt={name} width={250} height={200} />
       <h3>{name}</h3>
       <p>{info}</p>
+      <button onClick={handleSeeMoreClick}>Ver mas</button>
     </div>
   )
 }
