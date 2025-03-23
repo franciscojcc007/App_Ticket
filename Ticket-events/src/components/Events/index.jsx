fa70dee24341c59571cc6d1d1f78e244ab9af123
franciscoj <frankmazta@gmail.com>
2025-03-23 12:08:13 -0400

    Agregar funcionalidad de búsqueda en el componente Events

--- Ticket-events/src/components/Events/index.jsx
+++ Ticket-events/src/components/Events/index.jsx
@@ -1,21 +1,40 @@
 
-import EventItem from "./components/EventItem/index.jsx";
+import EventItem from "./components/EventItem";
 import data from "../../data/events.json";
 
 const events = data._embedded.events;
 
-const Events = () => {
+const Events = ({ searchTerm }) => {
+
+  const handleEventItemClick = (id) => {
+    console.log('Evento clickeado', id);
+  }
+
+  const renderEvent = () => {
+    let eventsFiltered = events;
+
+    if (searchTerm.length > 0) {
+      eventsFiltered = eventsFiltered.filter((item) =>
+        item.name.toLowerCase().includes(searchTerm))
+    }
+
+    return eventsFiltered.map((eventItem) => (
+      <EventItem
+        key={`event-item-${eventItem.id}`}
+        name={eventItem.name}
+        info={eventItem.info}
+        image={eventItem.images?.[0]?.url || 'ruta-a-imagen-default.jpg'}
+        onEventClick={handleEventItemClick}
+        id={eventItem.id}
+      />
+    ))
+
+  }
+
   return (
     <div>
       <h1>Eventos</h1>
-      {events.map((eventItem) => (
-        <EventItem
-          key={`event-item-${eventItem.id}`}
-          name={eventItem.name}
-          info={eventItem.info}
-          image={eventItem.images?.[0]?.url || 'ruta-a-imagen-default.jpg'}
-        />
-      ))}
+      {renderEvent()}
     </div>
   );
 };
